9a4906e8de085bfa70a2a81309eb75641949f697
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2021-07-15 21:33:39 +0100

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,14 +9,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  Student: Student={password:'k', linkedin:'i',location:'i' ,name: 'Mohsen', email:'asoas',tel:50666585,education:'',id:0,school:'INSAT',gender: "s", dateOfBirth: "2021-06-14", driverLicense: true,
+  /*Student: Student={password:'k', linkedin:'i',location:'i' ,name: 'Mohsen', email:'asoas',tel:50666585,education:'',id:0,school:'INSAT',gender: "s", dateOfBirth: "2021-06-14", driverLicense: true,
   languages: 
     ['french']}
   
     JobApplications:JobApplication []=[
     {appId: 5, motivation: 'tayara', appDate: '50', studentId:0 , jobOfferId:0, status:'accepted'},
     {appId: 5, motivation: 'tayara', appDate: '50', studentId:0 , jobOfferId:0, status:'accepted'}
-  ];
+  ];*/
   
   pic='';
 
@@ -25,24 +25,24 @@ export class ProfileComponent implements OnInit {
   constructor(private ActivatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-      this.ActivatedRoute.params.subscribe((params) => this.studentService.getApplicationsByStudentId(+params.id).subscribe(
+    /*  this.ActivatedRoute.params.subscribe((params) => this.studentService.getApplicationsByStudentId(+params.id).subscribe(
       (JobApplications) => this.JobApplications=JobApplications)
       )
-    ;
+    ;*/
 
-        this.ActivatedRoute.params.subscribe((params) => this.studentService.getStudentById(+params.id).subscribe(
+     /*   this.ActivatedRoute.params.subscribe((params) => this.studentService.getStudentById(+params.id).subscribe(
       (Student) => this.Student=Student)
       )
-    ;
+    ;*/
 
      
-      this.ActivatedRoute.params.subscribe((params) => this.studentService.getPicByStudentId(+params.id).subscribe(
+     /* this.ActivatedRoute.params.subscribe((params) => this.studentService.getPicByStudentId(+params.id).subscribe(
       (pic) => this.pic=pic)
       )
-    ;
+    ;*/
 
   }
-  ViewCV(){
+  /*ViewCV(){
      
       this.ActivatedRoute.params.subscribe((params) => this.studentService.getCvByStudentId(+params.id).subscribe(
       (CV) => {
@@ -75,7 +75,7 @@ export class ProfileComponent implements OnInit {
         }))
   
       }
-  }
+  }*/
   
 }
 
