0a1c09d9b1ec015bf575477070594e59c1f32983
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2021-07-15 20:16:47 +0100

    Delete src/app/Model directory

--- src/app/Model/models.ts
+++
@@ -1,7 +0,0 @@
-export * from './body';
-export * from './company';
-export * from './employer';
-export * from './jobApplication';
-export * from './jobOffer';
-export * from './student';
-export * from './user';
