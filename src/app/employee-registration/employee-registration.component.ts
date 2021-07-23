1f113bee2f8ee5e187958f619cb6733864cc1ec5
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2021-07-23 12:32:03 +0100

    service added

--- src/app/employee-registration/employee-registration.component.ts
+++ src/app/employee-registration/employee-registration.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-
+import { StudentService } from '../services/student.service';
+import { Student } from '../Models/student';
+import { UserService } from '../services/user.service';
+import { User } from '../Models/user';
 
 class ImageSnippet {
   constructor(public src: string, public file: File) {}
@@ -14,67 +17,25 @@ class ImageSnippet {
   styleUrls: ['./employee-registration.component.css']
 })
 export class EmployeeRegistrationComponent implements OnInit {
-  file:any=null;
-  constructor(private router:Router) { }
-  selectedFile: ImageSnippet;
 
-  fileName = '';
-  fileCV:any=null;
-
-  //student:Student=null;
-  //user:User=null;
+  constructor(private router:Router, private StudentService:StudentService, private userService:UserService) { }
 
 
   ngOnInit(): void {
   }
 
-  /*processFile(imageInput: any) {
-    this.file = imageInput.files[0];
-    const reader = new FileReader();
-
-    reader.addEventListener('load', (event: any) => {
-
-      this.selectedFile = new ImageSnippet(event.target.result, this.file);
-    });
-      reader.readAsDataURL(this.file);
-    
-      }
-
-
-      onFileSelected(event) {
-
-        this.fileCV = event.target.files[0];
-  
-        if (this.fileCV) {
-  
-            this.fileName = this.fileCV.name;
-  
-            const formData = new FormData();
-  
-            formData.append("pdf", this.fileCV);
-  
-        }
-    }
-
-
     onSubmit( loginForm: NgForm){
-      
-      
-      this.user=new User(0,loginForm.value.username,loginForm.value.email,loginForm.value.password,loginForm.value.tel,loginForm.value.gender);
-      this.student=new Student(this.user.id,loginForm.value.username,loginForm.value.email,loginForm.value.password,loginForm.value.tel,loginForm.value.gender,loginForm.value.Birthdate,false,'','','','','');
-      
-      this.userService.createUser(this.user).subscribe();
-      console.log(this.student);
-      this.StudentService.createStudentForm(this.student,this.fileCV,this.file).subscribe(
+
+      this.StudentService.createStudentForm(loginForm.value).subscribe(
         (data) => {
           alert("Vous êtes bien enregistré");
           this.router.navigate(['home']);
         },
-        (erreur) => alert("Veuillez réessayer votre enregistrement")
+        (erreur) => {
+        console.log(erreur);}
       )
-    }*/
+    }
 
 
 }
-
 
