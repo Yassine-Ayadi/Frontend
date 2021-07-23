4469f6e115e81d72ec3aa69091aceba5dbe3ea41
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2021-07-23 12:41:41 +0100

    Update student.service.ts

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+ import { Injectable } from '@angular/core';
 import  {basePath} from  "../constants"; 
 import { HttpClient, HttpHeaders, HttpParams, }  from '@angular/common/http';
 import { JobApplication } from '../model/jobApplication';
@@ -26,4 +26,189 @@ export class StudentService {
   public createStudentForm(loginForm: Student): Observable<Student> {
       return this.httpClient.post<Student>('http://localhost:3000/student',loginForm, httpOptions)
   }
+
+
+
+
+
+
+
+
+  public deleteCV(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling deleteCV.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<any>('delete',`${basePath}/students/${encodeURIComponent(String(id))}/cv`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public deletePic(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling deletePic.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<any>('delete',`${basePath}/students/${encodeURIComponent(String(id))}/pic`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public deleteStudent(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling deleteStudent.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<any>('delete',`${basePath}/students/${encodeURIComponent(String(id))}`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public getAllStudents(observe: any = 'body'): Observable<any> {
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<Student>('get',`${basePath}/students`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public getApplicationsByStudentId(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling getApplicationsByStudentId.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<JobApplication>('get',`${basePath}/student/${encodeURIComponent(String(id))}/applications`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public getCvByStudentId(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling getCvByStudentId.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<Blob>('get',`${basePath}/students/${encodeURIComponent(String(id))}/cv`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public getPicByStudentId(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling getPicByStudentId.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<Blob>('get',`${basePath}/students/${encodeURIComponent(String(id))}/pic`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public getStudentById(id: number): Observable<any> {
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling getStudentById.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<Student>('get',`${basePath}/students/${encodeURIComponent(String(id))}`,
+        {
+            headers: headers,
+        }
+    );
+  }
+
+  public updateCV(body: Object, id: number): Observable<any> {
+
+    if (body === null || body === undefined) {
+        throw new Error('Required parameter body was null or undefined when calling updateCV.');
+    }
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling updateCV.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<any>('put',`${basePath}/students/${encodeURIComponent(String(id))}/cv`,
+        {
+            body: body,
+            headers: headers,
+        }
+    );
+  }
+
+
+  public updatePic(body: Object, id: number): Observable<any> {
+
+    if (body === null || body === undefined) {
+        throw new Error('Required parameter body was null or undefined when calling updatePic.');
+    }
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling updatePic.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<any>('put',`${basePath}/students/${encodeURIComponent(String(id))}/pic`,
+        {
+            body: body,
+            headers: headers,
+        }
+    );
+  }
+
+  public updateStudent(body: Student, id: number): Observable<any> {
+
+    if (body === null || body === undefined) {
+        throw new Error('Required parameter body was null or undefined when calling updateStudent.');
+    }
+
+    if (id === null || id === undefined) {
+        throw new Error('Required parameter id was null or undefined when calling updateStudent.');
+    }
+
+    let headers = this.defaultHeaders;
+
+    return this.httpClient.request<any>('put',`${basePath}/students/${encodeURIComponent(String(id))}`,
+        {
+            body: body,
+            headers: headers,
+        }
+    );
+  }
+}
 
